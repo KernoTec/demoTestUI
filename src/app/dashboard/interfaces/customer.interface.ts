c5297d1e4c974479c317d762dd3ede63921d316e
Arnaldo Muñoz <kings31lendy@gmail.com>
2024-01-15 21:41:03 -0400

    Creando interfaces customers and vehicles

---
+++ src/app/dashboard/interfaces/customer.interface.ts
@@ -0,0 +1,10 @@
+export interface Customer{
+  id?:string;
+  nombre:string;
+  paterno:string;
+  materno:string;
+  tipoDocumento:string;
+  ci:string;
+  fechaNacimiento:string;
+  genero:string;
+}
---
+++ src/app/dashboard/interfaces/vehicle.interface.ts
@@ -0,0 +1,8 @@
+export interface Vehicle{
+  id?:string;
+  marca:string;
+  modelo:string;
+  anio:number;
+  placa:string;
+  customerId?:string;
+}
